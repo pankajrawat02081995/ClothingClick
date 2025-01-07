6dc6f38183985edf95300c17f9dd66ac2f879da2
Radhika Anand <radhikaanand4@gmail.com>
2025-01-07 13:34:38 +0530

    Resolved bugs

---
+++ Cloth App/ViewController/Post Item/RatingViewControlar/NewRatingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewRatingViewController.swift
+//  Cloth App
+//
+//  Created by Radhika Anand on 07/01/25.
+//
+
+import UIKit
+
+class NewRatingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
